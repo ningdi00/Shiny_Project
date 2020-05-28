ef44dda4dbd573afa61dfaa9acc257137efeba93
Di <ningdi00@hotmail.com>
2020-05-28 00:10:52 -0400

    Add new Violation/Daycare graph and cleaning comment

--- server.R
+++ server.R
@@ -1,120 +1,138 @@
 
-shinyServer(
-  
-  function(input, output, session){
-     
-    
-    
-     NYC_Daycare_Clean = reactive(
-        NYC_Daycare %>% 
-           mutate(., Inspection.month = floor_date(as.Date(clean_date_str(Inspection.Date)), 'month')) %>%
-           filter(!is.na(Inspection.month), !is.na(Violation.Category), !Violation.Category == "", Violation.Category == input$Vio.Cat) 
-     )
-   
-     output$table = DT::renderDataTable({
-        datatable(NYC_Daycare, rownames = FALSE, options = list(scrollX = TRUE)) %>% 
-           formatStyle(input$selected,
-                       background = "skyblue",
-                       fontWeight = 'bold')
-     })
-  
-     output$Boro_Bar = renderPlot({
-        NYC_Daycare_Clean() %>%
-           group_by(., Violation.Category, Borough) %>%
-           ggplot(aes(x = Borough, fill = Violation.Category)) +
-           geom_bar() +
-           labs(title = 'Violations by Borough',
-                x = 'Borough',
-                y = 'Violations') +
-           scale_fill_brewer(palette = 'Set3') +
-           theme_bw() +
-           theme(legend.key = element_blank())
-     })
-    
-     output$School_Type = renderPlot({
-        NYC_Daycare_Clean() %>% 
-           group_by(., Violation.Category, Child.Care.Type) %>%
-           ggplot(aes(x = Child.Care.Type, fill = Violation.Category)) +
-           geom_bar(position = 'dodge') +
-           labs(title = 'Violations School Type',
-                x = 'Child Care Type',
-                y = 'Violations') +
-           scale_fill_brewer(palette = 'Set3') +
-           theme_bw() +
-           theme(legend.key = element_blank())
-     })
-     
-     output$monthly_trend = renderPlot({
-       NYC_Daycare_Clean() %>%
-         group_by(Inspection.month) %>%
-         ggplot(aes(x = Inspection.month, fill = Violation.Category, group = Violation.Category)) +
-         geom_bar() +
-         labs(title = 'Violation by Time',
-              x = 'Time',
-              y = 'Violations') +
-         scale_fill_brewer(palette = 'Set3') + 
-         theme_bw() +
-         theme(legend.key = element_blank())
-     })
-   
-     output$Boro_Point = renderPlot({
-       NYC_Daycare_Clean() %>%
-         group_by(Borough, Violation.Category) %>%
-         summarise(Violation.Per.Daycare.B = n() / n_distinct(Full.Address)) %>%
-         ggplot(aes(x = Borough, y = Violation.Per.Daycare.B, color = Violation.Category, shaep = Violation.Category)) +
-         geom_point(size=6, show.legend = TRUE) +
-         labs(title = 'Violation Per Daycare by Borough',
-              x = 'Borough',
-              y = 'Violation Per Daycare') +
-         scale_color_brewer(palette = 'Set3') +
-         theme_bw() +
-         theme(legend.key = element_blank())
-     })
-   
-     output$Boro_Pie = renderPlot({
-       NYC_Daycare_Clean() %>% 
-         group_by(Borough) %>% 
-         summarise(Boro_Vio = n()) %>%
-         mutate(sum_Vio = sum(Boro_Vio), Boro_Precent = paste0(round(Boro_Vio/sum_Vio,2))) %>%
-         ggplot(aes(x = '', y = Boro_Vio, fill = Borough)) +
-         geom_bar(stat = 'identity', width = 1, color = 'white') +
-         coord_polar('y', start = 0) +
-         geom_text(aes(label = Boro_Precent), position = position_stack(vjust = 0.5)) +
-         labs(title = 'Violation Precent by Borough',
-              x = '',
-              y = 'Precentage %') +
-         scale_fill_brewer(palette = 'Set3') +
-         theme_bw() +
-         theme(axis.text = element_blank(),
-               axis.ticks = element_blank(),
-               panel.grid  = element_blank())
-     })
-   
-     output$Port_Image = renderImage({
-       image = (src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ3Q6T2MaUY6O1zEb1IJr0lYNkdPUUkTu07NpTkwtBQGCjb-oK-&usqp=CAU')
-     })
-     
-     url_me = a("https://www.linkedin.com/in/dning", 
-                href="https://www.linkedin.com/in/dning")
-     
-     output$web_me <- renderUI({
-       tagList("LinkedIn", url_me)
-     })
+shinyServer(function(input, output, session) {
 
-     url_NYC = a("https://data.cityofnewyork.us/Health/DOHMH-Childcare-Center-Inspections/dsg6-ifza", 
-                 href="https://data.cityofnewyork.us/Health/DOHMH-Childcare-Center-Inspections/dsg6-ifza")
-     
-     output$web_NYC <- renderUI({
-       tagList("NYC OpenData", url_NYC)
-     })
-     
-     # Output$NYC_Map = renderLeaflet({
-     #   
-     # })
-     # 
-     # output$Intro = renderText({
-     #   
-     # })
-     
-   }
- )
+  NYC_Daycare_Clean = reactive(
+    NYC_Daycare %>%
+      mutate(., Inspection.month = floor_date(as.Date(clean_date_str(Inspection.Date)), 'month')) %>%
+      filter(!is.na(Inspection.month), !is.na(Violation.Category), !Violation.Category == "", Violation.Category == input$Vio.Cat)
+  )
+  
+  output$table = DT::renderDataTable({
+    datatable(NYC_Daycare,
+              rownames = FALSE,
+              options = list(scrollX = TRUE)) %>%
+      formatStyle(input$selected,
+                  background = "skyblue",
+                  fontWeight = 'bold')
+  })
+  
+  output$Boro_Bar = renderPlot({
+    NYC_Daycare_Clean() %>%
+      group_by(., Violation.Category, Borough) %>%
+      ggplot(aes(x = Borough, fill = Violation.Category)) +
+      geom_bar() +
+      labs(title = 'Violations by Borough',
+           x = 'Borough',
+           y = 'Violations') +
+      scale_fill_brewer(palette = 'Set3') +
+      theme_bw() +
+      theme(legend.key = element_blank())
+  })
+  
+  output$School_Type = renderPlot({
+    NYC_Daycare_Clean() %>%
+      group_by(., Violation.Category, Child.Care.Type) %>%
+      ggplot(aes(x = Child.Care.Type, fill = Violation.Category)) +
+      geom_bar(position = 'dodge') +
+      labs(title = 'Violations School Type',
+           x = 'Child Care Type',
+           y = 'Violations') +
+      scale_fill_brewer(palette = 'Set3') +
+      theme_bw() +
+      theme(legend.key = element_blank())
+  })
+  
+  output$monthly_trend = renderPlot({
+    NYC_Daycare_Clean() %>%
+      group_by(Inspection.month) %>%
+      ggplot(aes(
+        x = Inspection.month,
+        fill = Violation.Category,
+        group = Violation.Category
+      )) +
+      geom_bar() +
+      labs(title = 'Violation by Time',
+           x = 'Time',
+           y = 'Violations') +
+      scale_fill_brewer(palette = 'Set3') +
+      theme_bw() +
+      theme(legend.key = element_blank())
+  })
+  
+  output$Boro_Point = renderPlot({
+    NYC_Daycare_Clean() %>%
+      group_by(Borough, Violation.Category) %>%
+      summarise(Violation.Per.Daycare.B = n() / n_distinct(Full.Address)) %>%
+      ggplot(
+        aes(
+          x = Borough,
+          y = Violation.Per.Daycare.B,
+          color = Violation.Category,
+          shaep = Violation.Category
+        )
+      ) +
+      geom_point(size = 6, show.legend = TRUE) +
+      labs(title = 'Violation Per Daycare by Borough',
+           x = 'Borough',
+           y = 'Violation Per Daycare') +
+      scale_color_brewer(palette = 'Set3') +
+      theme_bw() +
+      theme(legend.key = element_blank())
+  })
+  
+  output$Boro_Pie = renderPlot({
+    NYC_Daycare_Clean() %>%
+      group_by(Borough) %>%
+      summarise(Boro_Vio = n()) %>%
+      mutate(sum_Vio = sum(Boro_Vio),
+             Boro_Precent = paste0(round(Boro_Vio / sum_Vio, 2))) %>%
+      ggplot(aes(x = '', y = Boro_Vio, fill = Borough)) +
+      geom_bar(stat = 'identity',
+               width = 1,
+               color = 'white') +
+      coord_polar('y', start = 0) +
+      geom_text(aes(label = Boro_Precent), position = position_stack(vjust = 0.5)) +
+      labs(title = 'Violation Precent by Borough',
+           x = '',
+           y = 'Precentage %') +
+      scale_fill_brewer(palette = 'Set3') +
+      theme_bw() +
+      theme(
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        panel.grid  = element_blank())
+  })
+  
+  url_me = a("https://www.linkedin.com/in/dning",
+             href = "https://www.linkedin.com/in/dning")
+  
+  output$web_me <- renderUI({
+    tagList("LinkedIn", url_me)
+  })
+  
+  url_NYC = a(
+    "https://data.cityofnewyork.us/Health/DOHMH-Childcare-Center-Inspections/dsg6-ifza",
+    href = "https://data.cityofnewyork.us/Health/DOHMH-Childcare-Center-Inspections/dsg6-ifza"
+  )
+  
+  output$web_NYC <- renderUI({
+    tagList("NYC OpenData", url_NYC)
+  })
+  
+  output$By_aycare = renderPlot({
+    NYC_Daycare_Clean() %>%
+      group_by(Full.Address, ZipCode) %>%
+      filter(ZipCode %in% input$Zipcode) %>% 
+      summarise(Vio_Per_Daycare = n())%>%
+      ggplot(aes(x = Full.Address, y = Vio_Per_Daycare)) +
+      geom_col() +
+      coord_flip() +
+      labs(title = 'Violation by Individual Daycares',
+           x = 'Daycares',
+           y = 'Violations') +
+      theme_bw() +
+      theme(
+        panel.grid  = element_blank())
+  })
+  
+})
--- ui.R
+++ ui.R
@@ -1,88 +1,113 @@
 
+
 shinyUI(dashboardPage(
-  skin = "purple", #shiny ui skin color
+  skin = "purple",
+  #shiny ui skin color
   
   #header ####
-  dashboardHeader(title = h4(HTML('NYC Daycare Center Examination Analysis'))),
+  dashboardHeader(title = h4(
+    HTML('NYC Daycare Center Examination Analysis'))
+  ),
   
   #sidebar ####
   dashboardSidebar(
-    
     #sidebar menu ####
     sidebarMenu(
-      menuItem('Introduction', tabName = 'Intro', icon = icon('info-circle')),
+      menuItem(
+        'Introduction',
+        tabName = 'Intro',
+        icon = icon('info-circle')
+      ),
       menuItem('Graph', tabName = 'Graph', icon = icon('chart-bar')),
+      menuItem(
+        'Grapy by Daycare',
+        tabName = 'Graph_Daycare',
+        icon = icon('chart-line')
+      ),
       menuItem('Map', tabName = 'Map', icon = icon('map')),
       menuItem('Data Table', tabName = 'Data_Table', icon = icon('database')),
       menuItem('Reference', tabName = 'Reference', icon = icon('book'))
-      ),
-    
-    #sidebar Selection ####
-    selectizeInput(inputId = 'Vio.Cat', 
-                   label = 'Select Violation Type to Display', 
-                   choice = unique(NYC_Daycare[, 'Violation.Category']),
-                   multiple = TRUE,
-                   selected = list('GENERAL', 'CRITICAL', 'PUBLIC HEALTH HAZARD'))
-    
+    )
   ),
   
   #body ####
-  dashboardBody(
-    
-    tabItems(
-      
-      #Intro ####
-      tabItem(tabName = 'Intro',
-              fluidRow(
-                h1('NYC Daycare Center Examination Analysis'),
-                h3('"This dataset contains a list of all inspections conducted
+  dashboardBody(tabItems(
+    #Intro ####
+    tabItem(tabName = 'Intro',
+            fluidRow(
+              h1('NYC Daycare Center Examination Analysis'),
+              h3(
+                '"This dataset contains a list of all inspections conducted
                    and any associated violations at active, city-regulated,
                    center-based child care programs and summer camps over the past 3 years.
-                   The violations are pre-adjudicated and all individual citation instances of each violation are listed." 
-                   -- NYC OpenData'))
-              ),
-      
-      #Graph ####
-      tabItem(tabName = 'Graph',
-              fluidRow(
-                box(plotOutput('Boro_Bar')),
-                box(plotOutput('Boro_Pie'), width = 3),
-                box(plotOutput('Boro_Point'), width = 3)),
-              fluidRow(box(plotOutput('monthly_trend')),
-                       box(plotOutput('School_Type')))),
-      
-      #Data Table ####
-      tabItem(tabName = 'Data_Table',
-              fluidRow(box(DT::dataTableOutput('table'),
-                           width = 12))),
-      
-      
-      #About Me ####
-      tabItem(tabName = 'Reference',
-              fluidRow(
-                h1('About Me: '),
-                h3('Di Ning'),
-                uiOutput('web_me'),
-                h1(' '),
-                h1('References: '),
-                h3('Data Dictionary - Dataset Information'),
-                h4('Dataset Name - DOHMH Childcare Center Inspections'),
-                h4('Agency Name - Dataset Information'),
-                h4('Update Frequency- Daily'),
-                h4('Dataset Description - NYC regulated child care program inspection results'),
-                h4('Dataset Keywords - Dinspections, child, health'),
-                h4('Dataset Category - Health'),
-                uiOutput('web_NYC')
-              )
-      )
+                   The violations are pre-adjudicated and all individual citation instances of each violation are listed."
+                   -- NYC OpenData'
+              ))
+    ),
     
-    # violation graph by center (chain vs family owned)
-    # violation graph by inspection year/month
-    # violation filter by francise 
-    # violation by age range
-    # violation by program
-    # daycares map by boro
+    #Graph ####
+    tabItem(
+      tabName = 'Graph',
+      fluidRow(
+        selectizeInput(
+          inputId = 'Vio.Cat',
+          label = 'Select Violation Type to Display',
+          choice = unique(NYC_Daycare[, 'Violation.Category']),
+          multiple = TRUE,
+          selected = list('GENERAL', 'CRITICAL', 'PUBLIC HEALTH HAZARD')
+        ),
+        box(plotOutput('Boro_Bar')),
+        box(plotOutput('Boro_Pie'), width = 3),
+        box(plotOutput('Boro_Point'), width = 3)
+      ),
+      fluidRow(box(plotOutput('monthly_trend')),
+               box(plotOutput('School_Type')))
+    ),
+    
+    #Graph_Daycare ####
+    tabItem(
+      tabName = 'Graph_Daycare',
+      fluidRow(box(
+        pickerInput(
+          inputId = 'Zipcode',
+          label = 'Select Nearby Zip Code: ',
+          choices = unique(NYC_Daycare[, 'ZipCode']),
+          selected = 11101,
+          options = list(`actions-box` = TRUE),
+          multiple = TRUE
+        )
+      )),
+      box(plotOutput('By_aycare'), width = 12)
+    ),
+    
+    #Data Table ####
+    tabItem(tabName = 'Data_Table',
+            fluidRow(box(
+              DT::dataTableOutput('table'),width = 12))
+    ),
+    
+    
+    #About Me ####
+    tabItem(
+      tabName = 'Reference',
+      fluidRow(
+        h1('About Me: '),
+        h3('Di Ning'),
+        uiOutput('web_me'),
+        h1(' '),
+        h1('References: '),
+        h3('Data Dictionary - Dataset Information'),
+        h4('Dataset Name - DOHMH Childcare Center Inspections'),
+        h4('Agency Name - Dataset Information'),
+        h4('Update Frequency- Daily'),
+        h4('Dataset Description - NYC regulated child care program inspection results'),
+        h4('Dataset Keywords - Dinspections, child, health'),
+        h4('Dataset Category - Health'),
+        uiOutput('web_NYC')
+      )
+    )
+    
+    
   ))
-  
   
 ))
